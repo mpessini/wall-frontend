8fa292f4c38690dac75e6633aa504de826111e70
mpessini <marceloppessini@gmail.com>
2022-08-18 19:36:21 -0300

    pre-config for GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
-      <header>Wall App</header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <main>Wall</main>
+}
+
+export default Home
